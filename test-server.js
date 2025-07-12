128afd4ea649e61ab4f9c911415998d5b7916c21
tjkreddy <se22ucse272@mahindrauniversity.edu.in>
2025-07-12 09:04:35 +0530

    refactor: remove unused Mobile Floating Action Button for Admin Portal

--- test-server.js
+++ test-server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:8080'],
   credentials: true
 }));
 app.use(express.json());
